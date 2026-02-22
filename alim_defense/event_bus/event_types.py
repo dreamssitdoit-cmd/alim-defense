506d5af2cf937c1339eabb259a9d079dba9d95c3
Astral <astral@email.com>
2026-02-22 03:20:29 -0600

    feat(events): event taxonomy v1

---
+++ alim_defense/event_bus/event_types.py
@@ -0,0 +1,6 @@
+class EventTypes:
+    NETWORK_REQUEST = "network.request"
+    DNS_QUERY = "dns.query"
+    PROCESS_SPAWN = "process.spawn"
+    FILE_WRITE = "file.write"
+    ANOMALY_SCORE = "anomaly.score"
